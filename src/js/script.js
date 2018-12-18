23c12654160602eaa1a828272247055ac21172f2
gtrb <guillaume.trb@hotmail.com>
2018-12-18 23:41:05 +0100

    walls and character mouvement animation

--- src/js/script.js
+++ src/js/script.js
@@ -6,15 +6,14 @@ function game() {
   
   let player1 = document.querySelector('.player1');
   let player2 = document.querySelector('.player2');
-
+  const tiles = document.querySelectorAll('.game__tile');
+  const walls = document.querySelectorAll('.game__wall');
+  
   function initGame (){
-    const tiles = document.querySelectorAll('.game__tile');
-    const walls = document.querySelectorAll('.game__wall');
-    
-    //intialise les coordoonées
     let row = 1;
     let column = 1;
-    
+    vertical = true;
+
     //numérote les cases
     tiles.forEach(element => {
       element.addEventListener('click', function(){
@@ -31,6 +30,39 @@ function game() {
         row++;
       }
     })
+    
+    row = 1;
+    column = 1;
+    //numérote les cases murs
+    walls.forEach(item => {
+      // vertical permet de changer le nombre de colonnes qui n'est pas le même d'une ligne a l'autre.
+      if (vertical) {
+
+        if (column < 8) {
+          item.dataset.column = column;
+          column++;
+          item.dataset.row = row;
+        } else {
+          item.dataset.column = column;
+          item.dataset.row = row;
+          column = 1;
+          row++;
+          vertical = false;
+        }
+      } else {
+        if (column < 9) {
+          item.dataset.column = column;
+          column++;
+          item.dataset.row = row;
+        } else {
+          item.dataset.column = column;
+          item.dataset.row = row;
+          column = 1;
+          row++;
+          vertical = true;
+        }
+      }
+    });
   }
   
   //Vérifie l'accessibilité des cases autour du personnage
@@ -132,10 +164,124 @@ function game() {
 
     return true;
   }
+  //Gère le placement des murs
+
+  for (let i = 0; i < walls.length; i++) {
+    let wall = walls[i];
+    let testRow = wall.dataset.row;
+    let testColumn = wall.dataset.column;
+
+    //True = pas de collisions
+    function collide () {
+      if (wall.classList.contains('game__wall--clicked')  || wall.dataset.row > 16 || wall.dataset.column > 8) {
+        return false;
+      }
+      //Lignes
+      if (wall.dataset.row % 2 == 0) {
+        if (walls[i - 8].classList.contains('game__wall--clicked') && walls[i + 9].classList.contains('game__wall--clicked')) {
+          return false;
+        }
+        else if (walls[i+1].classList.contains('game__wall--clicked')){
+          return false;
+        }
+        else {
+          return true;
+        }
+      } else { // Colonnes
+        if (walls[i + 8].classList.contains('game__wall--clicked') && (walls[i + 9].classList.contains('game__wall--clicked'))) {
+          return false;
+        }
+        else if (walls[i+17].classList.contains('game__wall--clicked')){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+    }
+
+    function hover() {
+      console.log(wall);
+      wall.addEventListener('mouseover', function () {
+        if (wall.dataset.column > 8){
+          wall.classList.add('--hover');
+          walls[i - 1].classList.add('--hover');
+        }
+        else if (wall.dataset.row > 16){
+          wall.classList.add('--hover');
+          walls[i - 17].classList.add('--hover');
+          
+        }
+        else if (collide()) {
+          if (!wall.classList.contains('game__wall--clicked')) { //fonctionnement collide (éviter le double modificateur)
+            wall.classList.add('--hover');
+            if (wall.dataset.row % 2 == 0) { // Permet de savoir si il s'agit d'un mur vertical ou horizontal
+              walls[i + 1].classList.add('--hover');
+            } else {
+              walls[i + 17].classList.add('--hover');
+            }
+          }
+        }
+      });
+
+      wall.addEventListener('mouseout', function () {
+        if (wall.dataset.column > 8){
+          wall.classList.remove('--hover');
+          walls[i - 1].classList.remove('--hover');
+        }
+        else if (wall.dataset.row > 16){
+          wall.classList.remove('--hover');
+          walls[i - 17].classList.remove('--hover');
+          
+        }
+        else if (collide()) {
+          wall.classList.remove('--hover');
+          if (wall.dataset.row % 2 == 0) { // Permet de savoir si il s'agit d'un mur vertical ou horizontal
+              walls[i + 1].classList.remove('--hover');
+            } else {
+              walls[i + 17].classList.remove('--hover');
+            }
+        }
+      });
+    }
+
+    function click() {
+      wall.addEventListener('click', function () {
+        if (wall.dataset.column > 8){
+          wall.classList.add('game__wall--clicked');
+          walls[i - 1].classList.add('game__wall--clicked');
+          wall.classList.remove('--hover');
+          walls[i - 1].classList.remove('--hover');
+        }
+        else if (wall.dataset.row > 16){
+          wall.classList.add('game__wall--clicked');
+          walls[i - 17].classList.add('game__wall--clicked');
+          wall.classList.remove('--hover');
+          walls[i - 17].classList.remove('--hover');
+          
+        }
+        else if (collide()) {
+          wall.classList.add('game__wall--clicked');
+          wall.classList.remove('--hover');
+          
+          if (wall.dataset.row % 2 == 0) {
+            walls[i + 1].classList.add('game__wall--clicked');
+            walls[i + 1].classList.remove('--hover');
+
+          } else {
+            walls[i + 17].classList.add('game__wall--clicked');
+            walls[i + 17].classList.remove('--hover');
+          }
+        }
+      });
+    };
+    click();
+    hover();
+  }
 
   //GESTION DU CLIC
   function clickHandler(clicked) {
-    
+
     if (turn === 'player1') {
       if (clicked.firstElementChild == player1) {
         checkAround(player1);
